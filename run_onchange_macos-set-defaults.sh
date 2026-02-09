9a490377838efff4188da1d07fd04d826aa22290
Euiyub Jung <euiyub.jung@linecorp.com>
2026-02-09 15:06:47 +0900

    Add a command for Mission Control

--- run_onchange_macos-set-defaults.sh
+++ run_onchange_macos-set-defaults.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
-# Dock Configuration
+# Dock Configuration #
+# Enable 'Automatically hide and show the Dock'
 defaults write com.apple.dock autohide -bool true; killall Dock
+
+# Mission Control #
+# Disable 'Automatically rearrange Spaces based on most recent use'
+defaults write com.apple.dock mru-spaces -bool false; killall Dock
+
