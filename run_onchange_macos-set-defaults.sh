2d54760d5545968b93c678b141bff855d36a41d6
Euiyub Jung <euiyub.jung@linecorp.com>
2026-02-09 15:46:39 +0900

    Add command for Function keys

--- run_onchange_macos-set-defaults.sh
+++ run_onchange_macos-set-defaults.sh
@@ -12,9 +12,29 @@ defaults write com.apple.dock mru-spaces -bool false; killall Dock
 defaults write -g AppleLanguages -array "en" "ko"
 
 # Lock Screen #
+# Note: You need to manually set the message at 'Lock Screen > Show message when locked'
 # Always ask for password to unlock
 defaults write com.apple.screensaver askForPassword -int 1
 # Lock screen when idle for 1 minute, and apply it (after ;)
-defaults write com.apple.screensaver loginwindowIdle -int 1;
-/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+defaults write com.apple.screensaver loginwindowIdle -int 1
+
+# Keyboard #
+# Disable 'Correct spelling automatically'
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Disable 'Capatalize words automatically'
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+# Disable 'Add period with double-space'
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+# Disable 'Use smart quotes and dashed' for Apple apps
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+# Disable 'Use smart quotes and dashed' for System Settings
+defaults write NSGlobalDomain NSSmartQuotesAndDashesEnabled -bool false
+# Enable Keyboard navigation (Use keyboard navigation to move focus between conrtols)
+defaults write NSGlobalDomain NSKeyboardNavigationEnabled -bool true
+# Disable 'Press and Hold' (Not visible on System Settings)
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Function Keys #
+# Switch behavior of Function Keys to F1/F2/... as default
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
 
