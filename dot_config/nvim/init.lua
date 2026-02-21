746ba864d346ae7b8811d075b0f398063b9620de
Euiyub Jung <dwywdo.dev@gmail.com>
2026-02-22 00:48:01 +0900

    Disable swapfile on Neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,4 @@
+vim.opt.swapfile = false
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.highlights") -- Added to support Markdown heading highlight when cursor hovered
